65cd7579e5261d8882c95d0193b4d94bcc07ef1d
vignesh-kv <kv.vignesh2004@gmail.com>
2025-02-25 21:41:23 +0530

    Add files via upload
    ternary operator

---
+++ geo.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class geo{
+public static void main(String args[]){
+Scanner scan=new Scanner(System.in);
+boolean so=scan.nextBoolean();
+String result=so?"ghee":"butter";
+System.out.printf(result);
+}
+}
